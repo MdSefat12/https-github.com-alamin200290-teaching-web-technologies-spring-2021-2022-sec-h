fab0d2babd1e41b167f7b33075bc848a172b7201
Md Sefat <98251936+MdSefat12@users.noreply.github.com>
2022-02-08 14:54:34 +0600

    Update task4.php

--- MID_LAB_TASK_04_HTML_PHP/task4.php
+++ MID_LAB_TASK_04_HTML_PHP/task4.php
@@ -1,15 +1,18 @@
 <?php
-  $a = 50;
-  $b = 34;
-  $c = 22;
-  if($a >= $b){
-    if($a >= $c){
-      echo "$a is the largest number";
+  $r = 50;
+  $t = 34;
+  $h = 22;
+  if($r >= $t)
+  {
+    if($r >= $h)
+    {
+      echo "$t is the largest number";
     }
-    else echo "$c is the largest number";
+    else echo "$h is the largest number";
   }
-  else if($b >= $c){
-    echo "$b is the largest number";
+  else if($t >= $h)
+  {
+    echo "$t is the largest number";
   }
-  else echo "$c is the largest number";
+  else echo "$h is the largest number";
 ?>
